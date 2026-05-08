4c52a6aa503d5da7eaefeea937842637be729f66
Yunus Abdulhamid <yunusabdulhameed1@gmail.com>
2026-05-08 11:29:29 +0100

    fix sytax error in Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -78,7 +78,7 @@ const Login = () => {
                 type="email"
                 autoComplete="email"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 focus:z-10 sm:text-sm" {/* Emerald ring */}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-emerald-500 focus:border-emerald-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
                 value={formData.email}
                 onChange={handleChange}
